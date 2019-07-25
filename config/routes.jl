f4ab1c607f7235351e32da1da13b4ad1df5d548d
chaanks <jarod8405@gmail.com>
2019-07-25 17:05:12 +0200

    update Genie app

--- config/routes.jl
+++ config/routes.jl
@@ -1,4 +1,4 @@
-using Genie.Router
+using Genie.Router, Genie.WebChannels
 
 route("/") do
   serve_static_file("welcome.html")
@@ -10,4 +10,24 @@ end
 
 route("/error404") do
   error_404("the page you want")
+end
+
+
+route("/mychannel/subscribe") do
+  serve_static_file("test.html")
+end
+
+channel("/mychannel/subscribe") do 
+  WebChannels.subscribe(@params(:WS_CLIENT), "mychannel")
+  @show "Subscription OK"
+end
+
+channel("/mychannel/ping") do 
+  WebChannels.message(@params(:WS_CLIENT), "pong")
+  @show "pong OK"
+end
+
+function do_your_stuff_here()
+  # push updates
+  WebChannels.message("mychannel", json_payload)
 end
